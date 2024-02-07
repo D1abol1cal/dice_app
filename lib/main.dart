86ed53c7eb17a8be47441d538fe6cb9a28c66e59
Nofel <syednofeltalh2@gmail.com>
2024-02-07 07:22:56 +0500

    Custom Reusable Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 54, 89, 244),
-                Color.fromARGB(255, 33, 219, 243),
-                Colors.green,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello, World!",
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 54, 89, 244),
+            Color.fromARGB(255, 33, 219, 243),
+            Colors.green,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello, World!",
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
 }
